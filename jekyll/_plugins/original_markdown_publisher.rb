fcac91cb9cede5eec25824a83ea30bb1023aebd3
Forketyfork <forketyfork@icloud.com>
2025-11-12 08:11:39 +0100

    Fix llms.txt generation: proper URLs and multilingual support
    Fixed two critical issues:
    
    1. Broken markdown URLs: URLs were ending with /.md (e.g., /post/.md)
       creating hidden files. Now properly strips trailing slashes to
       generate URLs like /post.md
    
    2. Missing translations: Jekyll Polyglot filters posts by language
       during site generation. Plugin now directly reads all posts from
       file system across all language directories (en, de, ru) to ensure
       all translations are included in llms.txt
    
    The plugin now:
    - Reads posts from _posts/ and _posts/{lang}/ directories
    - Parses YAML frontmatter to extract metadata
    - Constructs proper URLs for each language
    - Generates correct .md file paths without trailing slashes
    - Includes all articles regardless of current language context

--- jekyll/_plugins/original_markdown_publisher.rb
+++ jekyll/_plugins/original_markdown_publisher.rb
@@ -1,10 +1,51 @@
 # frozen_string_literal: true
 
 require 'fileutils'
+require 'yaml'
+require 'date'
 
 module OriginalMarkdownPublisher
   def self.generate_llms_txt(site)
-    posts_by_lang = site.posts.docs.group_by { |post| post.data['lang'] || 'en' }
+    all_posts = if site.config['languages']
+                  languages = site.config['languages'] || ['en']
+                  languages.flat_map do |lang|
+                    posts_dir = if lang == site.config['default_lang']
+                                  File.join(site.source, '_posts')
+                                else
+                                  File.join(site.source, '_posts', lang)
+                                end
+
+                    next [] unless Dir.exist?(posts_dir)
+
+                    Dir.glob(File.join(posts_dir, '*.md')).map do |file|
+                      begin
+                        content = File.read(file)
+                        if content =~ /\A(---\s*\n.*?\n?)^((---|\.\.\.)\s*$\n?)/m
+                          data = YAML.safe_load(Regexp.last_match(1))
+                          {
+                            lang: data['lang'] || site.config['default_lang'],
+                            title: data['title'],
+                            date: data['date'],
+                            file: file
+                          }
+                        end
+                      rescue StandardError
+                        nil
+                      end
+                    end.compact
+                  end.compact
+                else
+                  site.posts.docs.map do |post|
+                    {
+                      lang: post.data['lang'] || 'en',
+                      title: post.data['title'],
+                      date: post.date,
+                      url: post.url
+                    }
+                  end
+                end
+
+    posts_by_lang = all_posts.group_by { |post| post[:lang] }
 
     content = []
     content << "# Forketyfork Dev Blog"
@@ -31,11 +72,27 @@ module OriginalMarkdownPublisher
       content << "## #{lang_name}"
       content << ""
 
-      posts = posts_by_lang[lang].sort_by { |p| p.date }.reverse
+      posts = posts_by_lang[lang].sort_by { |p| p[:date].is_a?(Date) ? p[:date] : Date.parse(p[:date].to_s) }.reverse
       posts.each do |post|
-        title = post.data['title']
-        date = post.date.strftime('%Y-%m-%d')
-        md_url = "#{site.config['url']}#{post.url}.md"
+        title = post[:title]
+        date_obj = post[:date].is_a?(Date) ? post[:date] : Date.parse(post[:date].to_s)
+        date = date_obj.strftime('%Y-%m-%d')
+
+        url = if post[:url]
+                post[:url]
+              else
+                filename = File.basename(post[:file], '.md')
+                match = filename.match(/^(\d{4})-(\d{2})-(\d{2})-(.+)$/)
+                slug = match[4]
+                if lang == 'en'
+                  "/blog/#{match[1]}/#{match[2]}/#{match[3]}/#{slug}/"
+                else
+                  "/#{lang}/blog/#{match[1]}/#{match[2]}/#{match[3]}/#{slug}/"
+                end
+              end
+
+        url = url.chomp('/')
+        md_url = "#{site.config['url']}#{url}.md"
         content << "- [#{title}](#{md_url}): #{date}"
       end
       content << ""
@@ -52,7 +109,7 @@ module OriginalMarkdownPublisher
       next unless source_path && File.exist?(source_path)
 
       original_content = File.binread(source_path)
-      relative_url = post.url.sub(%r{^/}, '')
+      relative_url = post.url.sub(%r{^/}, '').chomp('/')
       next if relative_url.empty?
 
       destination_path = File.join(site.dest, "#{relative_url}.md")
