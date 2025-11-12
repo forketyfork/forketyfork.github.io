96282955fce6a8afb7359abef30935b82a8fb246
Forketyfork <forketyfork@icloud.com>
2025-11-12 06:26:46 +0100

    Publish raw markdown versions of posts

---
+++ jekyll/_plugins/original_markdown_publisher.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require 'fileutils'
+
+module OriginalMarkdownPublisher
+  Jekyll::Hooks.register :site, :post_write do |site|
+    site.posts.docs.each do |post|
+      source_path = post.path
+      next unless source_path && File.exist?(source_path)
+
+      original_content = File.binread(source_path)
+      relative_url = post.url.sub(%r{^/}, '')
+      relative_url = relative_url.chomp('/')
+      next if relative_url.empty?
+
+      destination_path = File.join(site.dest, "#{relative_url}.md")
+      FileUtils.mkdir_p(File.dirname(destination_path))
+      File.binwrite(destination_path, original_content)
+    end
+
+    llms_source = File.join(site.source, 'llms.txt')
+    if File.exist?(llms_source)
+      llms_destination = File.join(site.dest, 'llms.txt')
+      FileUtils.mkdir_p(File.dirname(llms_destination))
+      FileUtils.cp(llms_source, llms_destination)
+    end
+  end
+end
